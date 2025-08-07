4230a57c92c595c3c29f943fe2553601f7dca5e3
ZaBuggy <za.buggy@yahoo.com>
2025-08-07 17:42:20 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+import os, random
+from datetime import datetime
+from collections import Counter
+
+app = Flask(__name__)
+
+ART_DIR = os.path.join("static", "artwork")
+LOG_FILE = "log.txt"
+
+@app.route("/")
+def random_art():
+    images = os.listdir(ART_DIR)
+    images = random.choice(images)
+
+    # Log the view
+    with open(LOG_FILE, "a") as f:
+        f.write(f"{datetime.now()} | {images}\n")
+
+    return render_template("art.html", image_file=images)
+
+@app.route("/stats")
+def view_stats():
+    if not os.path.exists(LOG_FILE):
+        return "No logs yet."
+
+    with open(LOG_FILE, "r") as f:
+        lines = f.readlines()
+
+    entries = [line.strip().split(" | ") for line in line if " | " in line]
+    image_counts = Counter(entry[1] for entry in entries)
+
+    return render_template("stats.html", logs=entries, counts=image_counts)
+
+if __name__ == "__main__":
+    app.run(debug=True)            
